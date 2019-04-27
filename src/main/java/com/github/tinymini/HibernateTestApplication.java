fd914a88515fea56c74f368894fbfed2ea786976
SiHyeong Kim <aberial@hotmail.com>
2019-04-27 17:58:36 +0900

    initial commit

---
+++ src/main/java/com/github/tinymini/HibernateTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.tinymini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateTestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/github/tinymini/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.github.tinymini;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HibernateTestApplication.class);
+	}
+
+}
